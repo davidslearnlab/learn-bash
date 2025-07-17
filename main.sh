b26eafe7fd251ce1d06436b0438ad3f0892e94c8
David Balishyan <davidbalishyan12@gmail.com>
2025-07-17 13:37:40 +0400

    Updated: Almost everything

--- main.sh
+++ main.sh
@@ -1,40 +1,147 @@
 #!/bin/bash
+# This is the shebang. It tells the system to use /bin/bash to interpret the script.
 
-# 0 to hero bash
-# examples in the BASH scripting language ans their explanations
+##########################################################
+# VARIABLES
+##########################################################
+# Variables are declared without a $ sign. Use $ to access them.
 
-# Print text using the "echo" command
-echo "Hello World" # <-- The string you want to print
+name="your_name"
+age=18 # Add your age
+echo "My name is $name and I am $age years old."
 
-# sleep/stop the program (mostly used for simulating delay)
-sleep 3 # <-- how many seconds you want the program to sleep/stop
+# Curly braces are useful when combining with strings
+echo "Uppercase version of name: ${name^^}"  # Converts to uppercase
+echo
 
-# Variables
-num = 10 # <-- the value of a variable
+##########################################################
+# ARITHMETIC OPERATIONS
+##########################################################
+# Use $(( expression )) for integer arithmetic
 
-# Read user input
-read number # <-- the variable name
+a=5
+b=3
+sum=$((a + b))
+echo "$a + $b = $sum"
+echo
 
-# Arguments
-nm = $1 # <-- position of the argument (after filename)
+##########################################################
+# CONDITIONAL STATEMENTS
+##########################################################
 
-# using command outputs
-user = $(whoami) # <-- the command, that you want to get the output
-
-# if/else/elseif statements
-if [ $num -gt 10 ]; then
-  echo "Number is greater than 10"
-elif [ $num -eq 10 ]; then
-  echo "Number is exactly 10"
+# IF-ELSE
+if [ $a -gt $b ]; then
+  echo "$a is greater than $b"
 else
-  echo "Number is less than 10"
+  echo "$a is not greater than $b"
+fi
+echo
+
+# CASE statement
+read -p "Enter a letter (a/b/c): " letter
+case $letter in
+  a) echo "You chose A" ;;
+  b) echo "You chose B" ;;
+  c) echo "You chose C" ;;
+  *) echo "Invalid choice" ;;
+esac
+echo
+
+##########################################################
+# LOOPS
+##########################################################
+
+# FOR loop
+echo "Counting from 1 to 5:"
+for i in {1..5}; do
+  echo "$i"
+done
+echo
+
+# WHILE loop
+echo "Countdown from 3:"
+count=3
+while [ $count -gt 0 ]; do
+  echo "$count"
+  count=$((count - 1))
+done
+echo
+
+##########################################################
+# FUNCTIONS
+##########################################################
+
+# Defining a function
+say_hello() {
+  echo "Hello, $1!"
+}
+
+# Calling the function with arguments
+say_hello "World"
+say_hello "your_name"
+echo
+
+##########################################################
+# FILE TESTING
+##########################################################
+
+file="test.txt"
+touch $file  # Create an empty file
+
+if [ -f "$file" ]; then
+  echo "$file exists and is a regular file."
 fi
 
-# Nested if statements
-if [ $num -gt 0 ]; then
-  if [ $num -lt 10 ]; then
-    echo "Number is between 1 and 9"
-  fi
+rm $file  # Clean up
+echo
+
+##########################################################
+# COMMAND SUBSTITUTION
+##########################################################
+# Use $(...) to capture the output of a command into a variable
+
+current_date=$(date)
+echo "Current date and time: $current_date"
+echo
+
+##########################################################
+# USER INPUT
+##########################################################
+
+read -p "What is your favorite language? " lang
+echo "You like $lang."
+echo
+
+##########################################################
+# EXIT STATUS AND ERROR HANDLING
+##########################################################
+
+mkdir some_dir
+if [ $? -eq 0 ]; then
+  echo "Directory created successfully."
+else
+  echo "Failed to create directory."
 fi
 
+rmdir some_dir
+echo
 
+##########################################################
+# SPECIAL VARIABLES
+##########################################################
+# $0 - name of the script
+# $1 to $9 - positional parameters
+# $# - number of arguments
+# $@ - all arguments
+# $? - exit status of the last command
+
+echo "Script name: $0"
+echo "Argument count: $#"
+echo "All arguments: $@"
+echo
+
+##########################################################
+# EXIT
+##########################################################
+
+exit 0
